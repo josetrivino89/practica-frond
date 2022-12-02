201702576e955101bf37d9b74949f1ca2799e523
josetrivino89 <josetrivino89@gmail.com>
2022-12-02 00:13:13 -0300

    se trabajo sobre el maquetado de las routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import { useState } from 'react'
 import './App.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
-import router from './router'
+import { Routes,Route,BrowserRouter,} from "react-router-dom";
+//importamos las rutas desde la carpeta router:
+import AdminUser from './pages/AdminUser';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import RoutePrivate from './router/routePrivate';
 
 function App() {
   
-
   return (
-    <RouterProvider router={router}/>   
+    <>
+    <p>Navbar</p>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/register" element={<Register/>}/>
+    <Route path="admin" element={
+      <RoutePrivate user={user}>
+      <AdminUser/>
+      </RoutePrivate>}/>
+    <Route path="*" element={<Error/>}/>
+    </Routes>
+    </BrowserRouter>
+    <p>Footer</p>
+    </>
  
  
   )
--- src/pages/columns.jsx
+++ src/pages/columns.jsx
@@ -1,13 +1,1 @@
 
-const columns = [{
-    dataField: 'id',
-    text: 'Product ID',
-  }, {
-    dataField: 'name',
-    text: 'Product Name',
-    filter: textFilter()
-  }, {
-    dataField: 'price',
-    text: 'Product Price',
-    filter: textFilter()
-  }];
