b86f4cc9644f5c646ee1308fe6cc7516e0c4550d
josetrivino89 <josetrivino89@gmail.com>
2022-12-05 15:13:55 -0300

    se trabajo sobre el loguin y en enlace con el back

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,34 @@
 import React, { useEffect } from 'react'
 import instance from '../api/axios'
+import {useNavigate} from 'react-router-dom'
 
 const Login = () => {
+    const navigate = useNavigate()
 
     const login = async()=>{
+
+        const user= {
+            email: "josethugo2@gmail.com",
+            password:"1234567"
+        }
         try {
-            const res= await instance.get("/users")
-            
+            const res= await instance.post("/auth/login", user)
+            const user_token= res.data.token
+            localStorage.setItem("token",user_token)
+            navigate ("/")            
         } catch (error) {
-            console.log(error);
-            
+            console.log(error);            
         }
         
-    }
-    useEffect(() => {
-      login()
-    
-      return () => {
-        
-      }
-    }, [])
-    
+    }   
   return (
     //1-Hacemos el formulario
     //2-con el usuario: nos logueamos con el mail y el passworr
     //3- si todo esta ok nos dan el token
-    <div>login</div>
+    <div>
+        <p>Login</p>
+         <button onClick={login}>entrar</button>
+    </div>
   )
 }
 
